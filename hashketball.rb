6f6f04afb2220bab8ce68ea6636df5c9046cd9a5
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-06 03:14:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,7 +55,7 @@ def game_hash
           blocks: 11,
           slam_dunks: 1   
         }
-      }
+      ]
         },
         away: {
           team_name: "Charlotte Hornets",
@@ -112,6 +112,5 @@ def game_hash
           slam_dunks: 12  
         }
         }
-  }
   }
 end
