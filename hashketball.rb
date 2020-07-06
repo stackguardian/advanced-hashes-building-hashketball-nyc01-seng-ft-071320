342755d8a4affe5b2ef8703b2a1b16b33ea92a15
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-06 03:36:53 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,21 +1,23 @@
 # Write your code here!
 def game_hash
   {
-    home: {
+   home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: {
-        "Alan Anderson" => {
-        number: 0,
-        shoe: 16,
-        points: 22,
-        rebounds: 12,
-        assists: 12,
-        steals: 3,
-        blocks: 1,
-        slam_dunks: 1 
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
         },
-        "Reggie Evans" => {
+        {
+          player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
           points: 12,
@@ -25,7 +27,8 @@ def game_hash
           blocks: 12,
           slam_dunks: 7
         },
-        "Brook Lopez" => {
+        {
+          player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
           points: 17,
@@ -35,17 +38,19 @@ def game_hash
           blocks: 1,
           slam_dunks: 15
         },
-        "Mason Plumee" => {
+        {
+          player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
           points: 26,
           rebounds: 11,
           assists: 6,
           steals: 3,
-          blocks: 8 ,
-          slam_dunks: 5 
+          blocks: 8,
+          slam_dunks: 5
         },
-        "Jason Terry" => {
+        {
+          player_name: "Jason Terry",
           number: 31,
           shoe: 15,
           points: 19,
@@ -53,15 +58,16 @@ def game_hash
           assists: 2,
           steals: 4,
           blocks: 11,
-          slam_dunks: 1   
+          slam_dunks: 1
         }
-      }
-        },
+      ]
+    },
         away: {
-          team_name: "Charlotte Hornets"
+          team_name: "Charlotte Hornets",
           colors: ["Turquoise", "Purple"],
-          players: {
-            "Jeff Adrien" => {
+          players: [
+            {
+       player_name: "Jeff Adrien", 
           number:4 ,
           shoe: 18,
           points: 10,
@@ -71,7 +77,8 @@ def game_hash
           blocks: 7,
           slam_dunks: 2  
         },
-        "Bismack Biyombo" => {
+        {
+       player_name:  "Bismack Biyombo", 
           number: 0,
           shoe: 16,
           points: 12,
@@ -81,7 +88,8 @@ def game_hash
           blocks: 15,
           slam_dunks: 10  
         },
-        "DeSagna Diop"
+        {
+        player_name: "DeSagna Diop",
         number: 2,
           shoe: 14,
           points: 24,
@@ -91,7 +99,8 @@ def game_hash
           blocks: 5,
           slam_dunks: 5  
         },
-        "Ben Gordon" => {
+        {
+       player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
           points: 33,
@@ -101,7 +110,8 @@ def game_hash
           blocks: 1,
           slam_dunks: 0  
         },
-        "Kemba Walker" => {
+        {
+        player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
@@ -111,7 +121,7 @@ def game_hash
           blocks: 5,
           slam_dunks: 12  
         }
-        }
+        ]
   }
   }
 end
