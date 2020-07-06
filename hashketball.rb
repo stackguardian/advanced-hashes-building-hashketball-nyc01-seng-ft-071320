892cf13929f5a068db355d3ace19cdf4f2d59f8e
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-06 03:09:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: {
+      players: 
         "Alan Anderson" => {
         number: 0,
         shoe: 16,
