49ae1029e05304211f87a5879956d9d71d12578d
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-06 03:13:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        "Alan Anderson" => [
+        "Alan Anderson" => {
         number: 0,
         shoe: 16,
         points: 22,
@@ -14,8 +14,8 @@ def game_hash
         steals: 3,
         blocks: 1,
         slam_dunks: 1 
-        ],
-        "Reggie Evans" => [
+        },
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -24,8 +24,8 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        ],
-        "Brook Lopez" => [
+        },
+        "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -34,8 +34,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        ],
-        "Mason Plumlee" => [
+        },
+        "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -44,8 +44,8 @@ def game_hash
           steals: 3,
           blocks: 8 ,
           slam_dunks: 5 
-        ],
-        "Jason Terry" => [
+        },
+        "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -54,14 +54,14 @@ def game_hash
           steals: 4,
           blocks: 11,
           slam_dunks: 1   
-        ]
+        }
       }
         },
         away: {
           team_name: "Charlotte Hornets",
           colors: ["Turquoise", "Purple"],
-          players: [
-            "Jeff Adrien" => [
+          players: {
+            "Jeff Adrien" => {
           number:4 ,
           shoe: 18,
           points: 10,
@@ -70,8 +70,8 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2  
-        ],
-        "Bismack Biyombo" => [
+        },
+        "Bismack Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -80,8 +80,8 @@ def game_hash
           steals: 22,
           blocks: 15,
           slam_dunks: 10  
-        ],
-        "DeSagna Diop" => [
+        },
+        "DeSagna Diop" => {
         number: 2,
           shoe: 14,
           points: 24,
@@ -90,8 +90,8 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5  
-        ],
-        "Ben Gordon" => [
+        },
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -100,8 +100,8 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0  
-        ],
-        "Kemba Walker" => [
+        },
+        "Kemba Walker" => {
           number: 33,
           shoe: 15,
           points: 6,
@@ -110,8 +110,8 @@ def game_hash
           steals: 7,
           blocks: 5,
           slam_dunks: 12  
-        ]
-        ]
-  ]
-  ]
+        }
+        }
+  }
+  }
 end
